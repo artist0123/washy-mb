1b31fb23c6ac7c0a7f83b4dba3bad34bcc914327
artist0123 <napatart1111@gmail.com>
2022-11-10 00:18:41 +0700

    carousel

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import 'react-native-gesture-handler';
 import { styled } from 'nativewind';
 import { NativeBaseProvider } from "native-base";
 import MainNavigator from './navigations/navigator';
+import { StatusBar } from 'expo-status-bar';
 
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <MainNavigator></MainNavigator>
+      <StatusBar />
     </NativeBaseProvider>
   );
 }
