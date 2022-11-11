0f361f25e2756dec0bd7092f02e104c382973797
artist0123 <napatart1111@gmail.com>
2022-11-11 18:52:37 +0700

    keen-slider

--- components/HomeCarousel.js
+++ components/HomeCarousel.js
@@ -1,31 +1,65 @@
 import React from "react";
-import Carousel from "react-native-reanimated-carousel";
-import { Dimensions } from "react-native";
-import { Text, Box, AspectRatio, Center } from "native-base";
+import { useKeenSliderNative } from "keen-slider/react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { Text, View, Image } from "native-base";
+
 const width = Dimensions.get("window").width;
 console.log(width);
 console.log((width / 16) * 9);
 export default function HomeCarousel() {
+  const slides = 4;
+  const slider = useKeenSliderNative({
+    slides,
+    loop: true,
+  });
   return (
-    <Carousel
-      loop
-      data={[...new Array(6).keys()]}
-      width={width}
-      style={{
-        width: width,
-        height: 200,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }}
-      autoPlayInterval={5000}
-      autoPlay={true}
-      renderItem={({ index }) => (
-        <AspectRatio ratio={{ base: 16/9 }} height={{ base: "100%" }}>
-          <Center bg="red.400">
-            <Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
-          </Center>
-        </AspectRatio>
-      )}
-    ></Carousel>
+    <View style={styles.slider} {...slider.containerProps}>
+      {[...Array(slides).keys()].map((key) => {
+        return (
+          <View key={key} {...slider.slidesProps[key]}>
+            <View style={{ ...styles.slide }}>
+              <Text style={styles.text}>Slide {key + 1}</Text>
+              {/* <Image
+                style={styles.img}
+                source={{
+                  uri: images[key],
+                }}
+              /> */}
+            </View>
+          </View>
+        );
+      })}
+    </View>
   );
 }
+
+const images = [
+  "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+];
+
+const styles = StyleSheet.create({
+  slider: {
+    backgroundColor: "#fff",
+    overflow: "hidden",
+    width: "100%",
+    height: (width / 16) * 9,
+  },
+  slide: {
+    width: "100%",
+    height: (width / 16) * 9,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  text: {
+    color: "white",
+    fontSize: 30,
+  },
+  bg: {
+    width: "100%",
+    height: (width / 16) * 9,
+  },
+});
