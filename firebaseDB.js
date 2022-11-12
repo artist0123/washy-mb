b7a354a8cf9299f7e5ed30eb9b6a97fca3ee66e1
artist0123 <napatart1111@gmail.com>
2022-11-12 20:23:44 +0700

    Create firebaseDB.js

---
+++ firebaseDB.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase/compat";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrKepIYYjhXMylM5gS9EVqq-F2FqqlLMg",
+  authDomain: "washy-mb.firebaseapp.com",
+  projectId: "washy-mb",
+  storageBucket: "washy-mb.appspot.com",
+  messagingSenderId: "1008506317149",
+  appId: "1:1008506317149:web:d4c1a4615a5601466260b4",
+  measurementId: "G-8G2763ZGY8",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+
